ba0cd629a58c6a3a723bfe917f57df1874b2cff9
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-08-10 17:01:09 +0530

    Resolved

--- Syncfusion_Sample/Syncfusion_Sample.UWP/UWPApplication.cs
+++ Syncfusion_Sample/Syncfusion_Sample.UWP/UWPApplication.cs
@@ -9,21 +9,21 @@ using System.Threading.Tasks;
 
 namespace Syncfusion_Sample.UWP
 {
-    public abstract class UWPApplication : MvxWindowsApplication<Setup, Core.App, FormsUI.App, MainPage>
+    public abstract class UWPApplication : MvxWindowsApplication<MVVMCrossSetup, Core.App, FormsUI.App, MainPage>
     {
 
     }
 
-    public class Setup : MvxFormsWindowsSetup<Core.App, FormsUI.App>
+    public class MVVMCrossSetup : MvxFormsWindowsSetup<Core.App, FormsUI.App>
     {
         public override IEnumerable<Assembly> GetViewAssemblies()
         {
-                var customAssemblies = new List<Assembly>()
+            var syncfusionChartRendererAssembly = new List<Assembly>()
                 {
                     typeof(Syncfusion.SfChart.XForms.UWP.SfChartRenderer).GetTypeInfo().Assembly
                 };
-                customAssemblies.AddRange(base.GetViewAssemblies());
-                return customAssemblies;
+            syncfusionChartRendererAssembly.AddRange(base.GetViewAssemblies());
+            return syncfusionChartRendererAssembly;
         }
     }
 }
